9b25aa3847c65bfa0c767f87b49cc7d3b28218d0
Enache Catalin <catalinenache03@gmail.com>
2018-09-18 14:23:24 -0700

    Access-Control-Allow-Origin enabled

--- app.js
+++ app.js
@@ -33,6 +33,24 @@ app.use(session({
 }))
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/public'));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use('/', indexRouter);
 app.use('/user',usersRouter);
 app.use('/preflight',preflightRouter);
